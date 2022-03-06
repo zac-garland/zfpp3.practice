079feb25c4fac8670cd25b18c179070c9ba7982a
Zac Garland <zachary.garland@outlook.com>
2022-03-05 22:13:54 -0700

    multi plot

--- notes/spx sectors.R
+++ notes/spx sectors.R
@@ -33,21 +33,39 @@ sector_index_chart <- function(df,start_date = (Sys.Date() %m-% years(3))){
     select(sector,ticker,date,adjusted) %>%
     filter(date >= as_date(start_date)) %>%
     mutate(key = glue::glue("{sector} | {ticker}")) %>%
+    mutate(year_mon = paste(week(date),year(date))) %>%
+    group_by(key,year_mon) %>%
+    filter(date == max(date,na.rm=TRUE)) %>%
     group_by(key) %>%
+    arrange(key,date) %>%
     mutate(index = adjusted/head(adjusted) - 1) %>%
     group_by(sector,date) %>%
     summarize(index = mean(index,na.rm=TRUE)) %>%
     ungroup() %>%
+    arrange(sector,date) %>%
     hchart("line",hcaes(date,index,group = sector)) %>%
     hc_add_theme(hc_theme_darkunica()) %>%
     hc_tooltip(shared = TRUE,table = TRUE,valueDecimals = 2,sort = TRUE) %>%
-    hc_rangeSelector(enabled = TRUE) %>%
-    hc_legend(enabled = FALSE)
+    hc_rangeSelector(enabled = TRUE)
 
 }
 
-sector_px %>%
-  sector_index_chart()
+
+plot_index_splines <- function(df = sector_px){
+  seq.Date((Sys.Date() - years(6)),Sys.Date(),by = "year") %>%
+    floor_date("year") %>%
+    rev() %>%
+    map(~{
+      df %>%
+        sector_index_chart(.x)
+
+    }) %>%
+    hw_grid()
+
+}
+
+plot_index_splines(sector_px)
+
 
 plot_stocks <- function(df){
   df %>% hchart("area",hcaes(date,total_ret,group = sector)) %>%
